6c8ec62e6d0fd849bfeb3476415865c608fc3712
VIVEK SHARMA <42120036+Vivek7866@users.noreply.github.com>
2020-06-10 19:46:17 +0530

    Add files via upload

---
+++ LPF_HPF.m
@@ -0,0 +1,13 @@
+
+% LPF and HPF on an image.
+% Created By : VIVEK SHARMA
+clc;
+clear;
+close all;
+I = imread('lena.jpg'); %taking image as input
+kernel = [-1 -1 -1;-1 8 -1;-1 -1 -1];%HPF
+kernel2 = 1/9 *([1,1,1;1,1,1;1,1,1]);%LPF
+LPF_Image = imfilter(I, kernel2, 'same');%using lpf on image
+HPF_Image = imfilter(I, kernel, 'same');%using hpf on image
+imshowpair(LPF_Image,HPF_Image,'montage');%showing both the image
+title('Low Pass Filter                                   High Pass Filter');
